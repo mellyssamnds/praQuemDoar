b13db426d930f9ada315f5109d322c230db9469c
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-12-15 17:50:13 -0300

    :construction: adiciona donorRoutes e institutionRoute

---
+++ src/routes/donorRoutes.js
@@ -0,0 +1,11 @@
+const controller = require('../controller/donor')
+const express = require('express')
+const router = express.Router()
+
+router.get('/all', controller.findAllDonors)
+router.get('/:id', controller.findDonorById)
+router.post('/add', controller.addNewDonor)
+router.patch('/:id', controller.updateDonor)
+router.delete('/:id', controller.deleteDonor)
+
+module.exports = router
