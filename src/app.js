f38dd8b2ee7910861314a3b438d4c0ff0e788dfa
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-12-17 01:40:26 -0300

    :recycle: refatora server.js e app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+require('dotenv').config()
 
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('./database/dbConnect')
+const donorRoutes = require('./routes/donorRoutes')
+const institutionRoutes = require('./routes/institutionRoute')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+mongoose.connect()
+
+app.use('/praquemdoar/donor', donorRoutes)
+app.use('/praquemdoar/institution', institutionRoutes)
+
+module.exports = app
